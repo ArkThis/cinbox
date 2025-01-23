9113e7b363da01e0e95682cc0fd570d85c6fec29
Peter B <pb@das-werkstatt.com>
2025-01-23 20:24:24 +0100

    Started CLI tool for creating CInbox-compatible tokens manually.

---
+++ src/helper/token.php
@@ -0,0 +1,128 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * This is a simple CLI helper to generate CInbox-compatible token files
+ * with a given set of targetFolders and an (item)ID.
+ */
+
+const MEMORY_TS_FORMAT = 'Ymd-His'; 
+
+// Exit codes:
+define('EXIT_OK', 0);
+define('EXIT_ERROR_ARGS', 10);
+define('EXIT_ERROR_INIT', 11);
+define('EXIT_ERROR_RUNTIME', 12);   // Unknown error during runtime.
+
+
+
+/**
+ * Parse the commandline arguments.
+ */
+function parseArgs()
+{
+    global $input, $output;
+
+    // Available commandline arguments (see showHelp() for details):
+    $shortOpts = "i:o:";
+    $longOpts = array(
+        'input ID',             // archive signature (input)
+        'output folder',        // output folder (where to create token)
+    );
+
+    $options = getopt($shortOpts, $longOpts);
+    if (empty($options) || isset($options['h'])) 
+    {
+        // TODO: Currently there's no syntax help ;)
+        return EXIT_OK;
+    }
+
+    // ------------------------------------------------
+    // Evaluate and set options:
+
+    if (isset($options['i']))
+    {
+        $input = $options['i'];
+        printf("Input: %s\n", $input);
+    }
+
+    if (isset($options['o']))
+    {
+        $output = $options['o'];
+        printf("Output: %s\n", $output);
+    }
+}
+
+
+/**
+ * Same as recall(), but formats the stored data in a more human-readable
+ * way. Translating the unix time to a formatted date/time string, and
+ * reformatting the information in a more "common" way.
+ */
+function recallNice($entry)
+{
+    // Recall entry (array) with unix-time keys:
+    //$entry = $this->recall($key, $strict, $unique);
+    $newEntries = array();   // Target array with "$new_key" format.
+
+    foreach ($entry as $unixTime => $value)
+    {
+        $timestamp = date(MEMORY_TS_FORMAT, $unixTime);
+
+        $newEntry = array();   // This is going out.
+        // Populate it properly:
+        $newEntry['timestamp'] = $timestamp;
+        $newEntry['unixTime'] = $unixTime;
+        $newEntry['value'] = $value;
+
+        $newEntries[] = $newEntry;
+    }
+
+    return $newEntries;
+}
+
+
+/**
+ * Returns a parse-able text that contains all desired information to be
+ * written in an item's "token" file for inter-process communication.
+ *
+ * @param[in] flags    See: https://www.php.net/manual/en/json.constants.php
+ */
+function getTokenData($flags=null, $itemId=null, $targetFolders=null)
+{
+    // TODO/FIXME:
+    // There should be a different variable for this, than just dumping the
+    // item's "memory" here...
+    //
+
+    $tokenData = array();
+    $tokenData['itemId'] = $itemId;
+    $tokenData['targetFolders'] = recallNice($targetFolders);
+
+    $json = json_encode($tokenData, $flags);
+
+    return $json;
+}
+
+
+/** ===========================================
+ *     MAIN
+ * ===========================================
+ */
+
+
+$input = null;
+$output = null;
+
+parseArgs();
+
+$targetFolders = array(
+    time() => $output
+);
+
+$token = getTokenData(null, $input, $targetFolders);
+printf("%s\n", $token);
+
+
+?>
+
