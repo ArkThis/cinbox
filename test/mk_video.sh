1ce46a37b44c1758d94b89f779ff6db3e4d4b14d
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2025-07-17 17:14:56 +0200

    Added sanity check and syntax help for stub-generator (mthk).

--- test/mk_video.sh
+++ test/mk_video.sh
@@ -7,20 +7,33 @@
 # The video files are complete dummies (random bytes).
 
 ITEMID="$1"
+
 DIR_HIRES="$ITEMID/hires"
 DIR_LOWRES="$ITEMID/lowres"
 DIR_METADATA="$ITEMID/metadata"
 
 STUBSIZE="16k"
 
+if [ -z "$ITEMID" ]; then
+    echo "ERROR: No Item-ID given."
+    echo ""
+    echo "SYNTAX:  $0 ITEMID"
+    echo "Example: $0 vx-00815"
+    exit 1
+fi
+
 echo "Creating folders..."
-mkdir -v $ITEMID
-mkdir $DIR_HIRES
-mkdir $DIR_LOWRES
+mkdir -v $ITEMID            # One parent folder per-id
+mkdir -v $DIR_HIRES         # archival copy
+mkdir -v $DIR_LOWRES        # access copy
+mkdir -v $DIR_METADATA      # all things metadata
 mkdir -p $DIR_METADATA/images/minutes
 mkdir -p $DIR_METADATA/images/scene-cuts
 
-echo "Generating hires video stub files..."
+# Uncomment this to pause before creating anything:
+read -p "Press any key to continue..."
+
+echo "Generating video stub files..."
 for i in $(seq 0 10); do
     STUB=$DIR_HIRES/$(printf "${ITEMID}_%03d.avi" $i)
     echo -n "$i, "
