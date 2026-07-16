9d2ccc3a10b9e4f5890ea7fd7a4513fef80816e9
Peter Bubestinger-Steindl <pb@das-werkstatt.com>
2026-07-16 13:06:12 +0200

    Added missing php packages to setup script (mbstring, intl).

--- setup.sh
+++ setup.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 # @author: Peter B.
+# @date: 2026-07-16
 # @description:
 #   Install and handle pre-requisites and installed packages for CInbox to
 #   setup and function correctly.
 
 
-# Required packages:
+# Required system packages:
 # =======================
-# coreutils is needed for `cut` command. Should be present out-of-the-box,
+# NOTE: 'coreutils' is needed for `cut` command. Should be present out-of-the-box,
 # mostly. ;) Thx GNU!
-PACKAGES="git php-cli composer coreutils"
+
+PACKAGES="git php-cli php-mbstring php-intl coreutils"
+echo ""
+echo "Installing the following packages:"
+echo "  $PACKAGES"
+echo ""
+read -p "Press any key to continue"
 sudo apt install $PACKAGES
+
+
+
+# External PHP composer dependencies
+# ========================================
 
 # These programs need to be run to prepare composer autoload and dependency handling:
 # This is /NOT/ run as root, but as the user you'll be running the CInbox with daily.
-composer install
+#composer install
 
 # ------------------ This may be OBSOLETE?
 # Pull required dependency for parsing cron-like syntax:
